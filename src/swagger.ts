e67ed4eba2a86606c1ab4bd5127a6abc62015a9b
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-11-30 02:14:02 -0600

    Update Docs

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,16 +1,20 @@
 import swaggerJsdoc from 'swagger-jsdoc'
 
 const options = {
-	definition: {
-		openapi: '3.0.0',
-		info: {
-			title: 'API REST',
-			version: '1.0.0',
-			description: 'Documentación de la API REST',
-		},
-		servers: [{ url: 'http://localhost:3000' }],
-	},
-	apis: ['./src/api/*.ts'],
+  // 'definition' es requerido por swagger-jsdoc.
+  // Actúa como la base de la especificación.
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'SocketLink API', // Este título será sobrescrito por swagger.yml
+      version: '1.0.0',      // Esta versión también será sobrescrita
+    },
+  },
+  // Se le indica a swagger-jsdoc que procese todos los archivos YAML.
+  // 1. swagger.yml: Aporta la información base (info, servers).
+  // 2. components.yml: Aporta todos los componentes reusables.
+  // 3. paths/**/*.yml: Aporta todos los endpoints.
+  apis: ['src/swagger/swagger.yml', 'src/swagger/components.yml', 'src/swagger/paths/**/*.yml'],
 };
 
 const swaggerSpec = swaggerJsdoc(options);
